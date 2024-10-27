1a73edc3fba564b28c4dde950c2f73718f687f85
Alec <alecsharpie@gmail.com>
2024-10-27 22:16:47 +1100

    tidy

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from finance.db import FinanceDB
 
+db = FinanceDB('data/finance-stag.db')
+
 def get_schema():
-    db = FinanceDB('data/finance-stag.db')
-    # query = "SELECT * FROM transactions"
     query = "PRAGMA table_info(transactions)"
     result = db.run_query_pandas(query)
     print(result)
 
 def get_recurring_transactions():
-    db = FinanceDB('data/finance-stag.db')
-    # SQL query to find recurring transactions
     query = """
     SELECT 
         merchant_name, 
@@ -26,15 +24,10 @@ def get_recurring_transactions():
         occurrence_count DESC;
     """
 
-    # Execute the query and fetch the results
     result = db.run_query_pandas(query)
-
-    # Print the results
     print(result)
     
 def get_count_of_each_merchant():
-    db = FinanceDB('data/finance-stag.db')
-    # SQL query to find the count of each merchant
     query = """
     SELECT 
         merchant_name, 
@@ -47,10 +40,7 @@ def get_count_of_each_merchant():
         transaction_count DESC;
     """
 
-    # Execute the query and fetch the results
     result = db.run_query_pandas(query)
-
-    # Print the results
     print(result)
 
 if __name__ == "__main__":
